5b853357cd9d5a7a86513a9c89f4a95f33d13501
Tatyana Kostyrkina <tatyana.kostyrkina@zone3000.net>
2018-01-16 15:11:19 +0200

    lecture3

---
+++ TestLect3-1.js
@@ -0,0 +1,22 @@
+let fruit = 'orange';
+if (true) {
+    let fruit = 'apple';
+    console. log(fruit);
+} 
+console. log(fruit);
+
+let username;
+function setUsername(name){
+    username = name;
+}
+
+function getUsername(){
+    return username
+}
+
+const pi = 3.14;
+if (true){
+    const pi = 3.1415;
+    console. log(pi);
+}
+console. log(pi);
